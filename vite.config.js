3499e208424d82bd934c3a516171be8336b5d071
uikang1 <dasuu12@gmail.com>
2024-07-02 16:06:36 +0900

    Update deployment settings

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: '/madmove/',
     plugins: [react()],
-    server: {
-        port: 3000,
-    },
 });
